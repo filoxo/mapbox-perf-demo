8e00deaa57f89f0a90737461348334183fa96611
Carlos Filoteo <cfiloteo@ancestry.com>
2017-04-26 10:20:36 -0600

    Add MarkerMap and PopupMap components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MarkerMapComponent } from './marker-map/marker-map.component';
+import { PopupMapComponent } from './popup-map/popup-map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MarkerMapComponent,
+    PopupMapComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/marker-map/marker-map.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-marker-map',
+  templateUrl: './marker-map.component.html',
+  styleUrls: ['./marker-map.component.css']
+})
+export class MarkerMapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
