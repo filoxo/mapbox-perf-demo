439497de57eb476dbbab6e6b108c19c441b81922
Carlos Filoteo <cfiloteo@ancestry.com>
2017-04-26 10:20:43 -0600

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { MarkerMapComponent } from './marker-map/marker-map.component';
+import { PopupMapComponent } from './popup-map/popup-map.component';
+
 const routes: Routes = [
   {
-    path: '',
-    children: []
+    path: 'markers',
+    component: MarkerMapComponent
+  },
+  {
+    path: 'popups',
+    component: PopupMapComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'markers'
   }
 ];
 
