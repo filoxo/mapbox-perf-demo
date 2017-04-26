0f08ee11f5ac11ab4ebba30a405208c08a9d0004
Carlos Filoteo <cfiloteo@ancestry.com>
2017-04-26 16:27:44 -0600

    Add popup elments

--- src/app/popup-map/popup-map.component.ts
+++ src/app/popup-map/popup-map.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+import { timePeriod } from '../data.mock';
+import { MapComponent } from '../shared/map/map.component';
 
 @Component({
   selector: 'app-popup-map',
@@ -6,12 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./popup-map.component.css']
 })
 export class PopupMapComponent implements OnInit {
+  @ViewChild('map') map: MapComponent;
+  @ViewChild('ancestorDetail') ancestorDetailPopover: ElementRef;
+  @ViewChild('ancestorList') ancestorListPopover: ElementRef;
+
+  ancestors: any[];
+
+  private popup: mapboxgl.Popup;
+  private selectedAncestor: any;
 
   constructor() { }
 
   ngOnInit() {
+    this.ancestors = timePeriod.ancestors;
   }
-  initializeMap() {
-    console.log('popup-map initialized');
+
+  selectAncestor(ancestor) {
+    if (this.popup !== undefined && this.popup.isOpen()) {
+      this.popup.remove();
+    }
+    this.selectedAncestor = ancestor;
+    this.popup = new mapboxgl.Popup({ offset: { 'top': [-3, -10], 'bottom': [-3, -40] } })
+      .setLngLat([-96, 37.8])
+      .setDOMContent(this.ancestorDetailPopover.nativeElement)
+      .addTo(this.map.map);
+    this.popup.once('close', event => {
+      this.selectedAncestor = null;
+    });
   }
 }
