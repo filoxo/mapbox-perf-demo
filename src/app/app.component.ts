55d4151d156bc482f8bc696d2efc27dbc5b11c02
Carlos Filoteo <cfiloteo@ancestry.com>
2017-04-26 13:30:50 -0600

    Add version and supported values to App component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+declare var mapboxgl: any;
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  version: string;
+  supported: string;
+  ngOnInit() {
+    this.version = mapboxgl.version;
+    this.supported = mapboxgl.supported().toString();
+  }
 }
